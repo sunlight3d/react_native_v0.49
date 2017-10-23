7ca90db3c064548798a9436b69d4f12e448df20b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-10-23 22:12:35 +0700

    update

---
+++ 15-ScrollView#2.Draw a horizontal ScrollView like Page/code/tutorialProject/index.js
@@ -0,0 +1,13 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Email: sunlight4d@gmail.com
+Draw a basic ScrollView with mixed Components
+*/
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import VerticalScrollView from './components/VerticalScrollView';
+import HorizontalScrollView from './components/HorizontalScrollView';
+
+AppRegistry.registerComponent('tutorialProject', () => HorizontalScrollView);
