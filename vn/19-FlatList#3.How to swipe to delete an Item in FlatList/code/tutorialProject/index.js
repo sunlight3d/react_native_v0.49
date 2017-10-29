6d82955d379f015b3c91595cf824ca2381ea9e9c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-10-29 20:25:03 +0700

    update

---
+++ vn/19-FlatList#3.How to swipe to delete an Item in FlatList/code/tutorialProject/index.js
@@ -0,0 +1,13 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Email: sunlight4d@gmail.com
+Create a basic FlatList which contains Text
+*/
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+// import App from './App';
+import BasicFlatList from './components/BasicFlatList';
+
+AppRegistry.registerComponent('tutorialProject', () => BasicFlatList);
