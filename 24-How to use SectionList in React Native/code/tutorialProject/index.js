099fa147e2b18493db37bb90a0e5185514186a40
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-11-05 13:57:21 +0700

    update

---
+++ 24-How to use SectionList in React Native/code/tutorialProject/index.js
@@ -0,0 +1,15 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Email: sunlight4d@gmail.com
+An example of SectionList like IT's fields
+*/
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+// import App from './App';
+// import BasicFlatList from './components/BasicFlatList';
+// import HorizontalFlatList from './components/HorizontalFlatList';
+import BasicSectionList from './components/BasicSectionList';
+
+AppRegistry.registerComponent('tutorialProject', () => BasicSectionList);
