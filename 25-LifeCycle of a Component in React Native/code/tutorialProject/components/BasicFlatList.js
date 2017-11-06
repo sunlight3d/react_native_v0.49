bb5947a0101a8f0e0960d4577fe1aad4b37dfa7a
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-11-06 23:28:29 +0700

    update

---
+++ 25-LifeCycle of a Component in React Native/code/tutorialProject/components/BasicFlatList.js
@@ -0,0 +1,182 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Email: sunlight4d@gmail.com
+FlatList Component with Images
+Add newFood to FlatList
+*/
+import React, { Component } from 'react';
+import { AppRegistry, FlatList, StyleSheet, Text, View, Image, Alert, Platform, TouchableHighlight } from 'react-native';
+import flatListData from '../data/flatListData';
+import Swipeout from 'react-native-swipeout';
+
+import AddModal from './AddModal';
+import EditModal from './EditModal';
+
+class FlatListItem extends Component {
+    constructor(props) {
+        super(props);   
+        this.state = {
+            activeRowKey: null,
+            numberOfRefresh: 0
+        };          
+    }
+    refreshFlatListItem = () => {
+        this.setState((prevState) => {
+            return {
+                numberOfRefresh: prevState.numberOfRefresh + 1
+            };
+        });        
+    }
+    render() {   
+        const swipeSettings = {
+            autoClose: true,
+            onClose: (secId, rowId, direction) => {
+                if(this.state.activeRowKey != null) {
+                    this.setState({ activeRowKey: null });
+                }              
+            },          
+            onOpen: (secId, rowId, direction) => {
+                this.setState({ activeRowKey: this.props.item.key });
+            },      
+            right: [
+                { 
+                    onPress: () => {                            
+                        // alert("Update");
+                        this.props.parentFlatList.refs.editModal.showEditModal(flatListData[this.props.index], this);
+                    }, 
+                    text: 'Edit', type: 'primary' 
+                },
+                { 
+                    onPress: () => {    
+                        const deletingRow = this.state.activeRowKey;          
+                        Alert.alert(
+                            'Alert',
+                            'Are you sure you want to delete ?',
+                            [                              
+                              {text: 'No', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                              {text: 'Yes', onPress: () => {        
+                                flatListData.splice(this.props.index, 1); 
+                                //Refresh FlatList ! 
+                                this.props.parentFlatList.refreshFlatList(deletingRow);
+                              }},
+                            ],
+                            { cancelable: true }
+                          ); 
+                    }, 
+                    text: 'Delete', type: 'delete' 
+                }
+            ],  
+            rowId: this.props.index, 
+            sectionId: 1    
+        };               
+        return (  
+            <Swipeout {...swipeSettings}>
+                <View style={{
+                flex: 1,
+                flexDirection:'column',                                
+                }}>            
+                    <View style={{
+                            flex: 1,
+                            flexDirection:'row',
+                            // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen': 'tomato'                
+                            backgroundColor: 'mediumseagreen'
+                    }}>            
+                        <Image 
+                            source={{uri: this.props.item.imageUrl}}
+                            style={{width: 100, height: 100, margin: 5}}
+                        >
+
+                        </Image>
+                        <View style={{
+                                flex: 1,
+                                flexDirection:'column',   
+                                height: 100                 
+                            }}>            
+                                <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+                                <Text style={styles.flatListItem}>{this.props.item.foodDescription}</Text>
+                        </View>              
+                    </View>
+                    <View style={{
+                        height: 1,
+                        backgroundColor:'white'                            
+                    }}>
+                
+                    </View>
+                </View>   
+            </Swipeout>      
+            
+        );
+    }
+}
+const styles = StyleSheet.create({
+    flatListItem: {
+        color: 'white',
+        padding: 10,
+        fontSize: 16,  
+    }
+});
+
+export default class BasicFlatList extends Component {
+    constructor(props) {
+        super(props);     
+        this.state = ({
+            deletedRowKey: null,            
+        });
+        this._onPressAdd = this._onPressAdd.bind(this);        
+    }
+    refreshFlatList = (activeKey) => {
+        this.setState((prevState) => {
+            return {
+                deletedRowKey: activeKey
+            };
+        });
+        this.refs.flatList.scrollToEnd();
+    }
+    _onPressAdd () {
+        // alert("You add Item");
+        this.refs.addModal.showAddModal();
+    }
+    render() {
+      return (
+        <View style={{flex: 1, marginTop: Platform.OS === 'ios' ? 34 : 0}}>
+            <View style={{
+                backgroundColor: 'tomato', 
+                flexDirection: 'row',
+                justifyContent:'flex-end',                
+                alignItems: 'center',
+                height: 64}}>
+                <TouchableHighlight 
+                    style={{marginRight: 10}}
+                    underlayColor='tomato'
+                    onPress={this._onPressAdd}
+                    >
+                <Image
+                    style={{width: 35, height: 35}}
+                    source={require('../icons/icons-add.png')}
+                />
+            </TouchableHighlight>
+            </View>
+            <FlatList 
+                ref={"flatList"}
+                data={flatListData}
+                renderItem={({item, index})=>{
+                    //console.log(`Item = ${JSON.stringify(item)}, index = ${index}`);
+                    return (
+                    <FlatListItem item={item} index={index} parentFlatList={this}>
+
+                    </FlatListItem>);
+                }}
+                >
+
+            </FlatList>
+            <AddModal ref={'addModal'} parentFlatList={this} >
+
+            </AddModal>
+            <EditModal ref={'editModal'} parentFlatList={this}>
+
+            </EditModal>
+        </View>
+      );
+    }
+}
---
+++ 25-LifeCycle of a Component in React Native/code/tutorialProject/components/EditModal.js
@@ -0,0 +1,123 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Email: sunlight4d@gmail.com
+FlatList Component with Images
+Present Modal and update an existing FlatList's Item
+*/
+import React, { Component } from 'react';
+import {
+    AppRegistry, FlatList, StyleSheet, Text, View, Image, Alert,
+    Platform, TouchableHighlight, Dimensions,
+    TextInput
+} from 'react-native';
+import Modal from 'react-native-modalbox';
+import Button from 'react-native-button';
+import flatListData from '../data/flatListData';
+
+var screen = Dimensions.get('window');
+export default class EditModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foodName: '',
+            foodDescription: ''
+        };
+    }
+    showEditModal = (editingFood, flatlistItem) => {     
+        // console.log(`editingFood = ${JSON.stringify(editingFood)}`);           
+        this.setState({
+            key: editingFood.key,
+            foodName: editingFood.name,
+            foodDescription: editingFood.foodDescription,
+            flatlistItem: flatlistItem
+        });
+        this.refs.myModal.open();
+    } 
+    generateKey = (numberOfCharacters) => {
+        return require('random-string')({length: numberOfCharacters});        
+    }
+    render() {
+        return (
+            <Modal
+                ref={"myModal"}
+                style={{
+                    justifyContent: 'center',
+                    borderRadius: Platform.OS === 'ios' ? 30 : 0,
+                    shadowRadius: 10,
+                    width: screen.width - 80,
+                    height: 280
+                }}
+                position='center'
+                backdrop={true}
+                onClosed={() => {
+                    // alert("Modal closed");
+                }}
+            >
+                <Text style={{
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    marginTop: 40
+                }}>food's information</Text>
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderBottomColor: 'gray',
+                        marginLeft: 30,
+                        marginRight: 30,
+                        marginTop: 20,
+                        marginBottom: 10,
+                        borderBottomWidth: 1
+                    }}           
+                    onChangeText={(text) => this.setState({ foodName: text })}
+                    placeholder="Enter food's name"
+                    value={this.state.foodName}                 
+                />
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderBottomColor: 'gray',
+                        marginLeft: 30,
+                        marginRight: 30,
+                        marginTop: 10,
+                        marginBottom: 20,
+                        borderBottomWidth: 1
+                    }}
+                    
+                    onChangeText={(text) => this.setState({ foodDescription: text })}
+                    placeholder="Enter food's description"
+                    value={this.state.foodDescription}
+                />
+                <Button
+                    style={{ fontSize: 18, color: 'white' }}
+                    containerStyle={{
+                        padding: 8,
+                        marginLeft: 70,
+                        marginRight: 70,
+                        height: 40,
+                        borderRadius: 6,
+                        backgroundColor: 'mediumseagreen'
+                    }}
+                    onPress={() => {
+                         if (this.state.foodName.length == 0 || this.state.foodDescription.length == 0) {
+                            alert("You must enter food's name and description");
+                            return;
+                        }       
+                        //Update existing Food
+                        var foundIndex = flatListData.findIndex(item => this.state.key == item.key);
+                        if (foundIndex < 0) {
+                            return; //not found
+                        }
+                        flatListData[foundIndex].name = this.state.foodName;
+                        flatListData[foundIndex].foodDescription = this.state.foodDescription;
+                        //Refresh flatlist item
+                        this.state.flatlistItem.refreshFlatListItem();
+                        this.refs.myModal.close();                                                                       
+                    }}>
+                    Save
+                </Button>
+            </Modal>
+        );
+    }
+}
