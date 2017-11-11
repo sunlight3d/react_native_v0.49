b84dda70e044b8ee408ed910488df4b9808b8fd5
Nguyen Duc Hoang <sunlight4d@gmail.com>
2017-11-11 10:54:50 +0700

    update

---
+++ 27-Networking#2. Send POST request to insert data to database server/code/tutorialProject/components/AddModal.js
@@ -0,0 +1,123 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Email: sunlight4d@gmail.com
+FlatList Component with Images
+Present Modal and update an existing FlatList's Item
+*/
+import React, { Component } from 'react';
+import {
+    AppRegistry, FlatList, StyleSheet, Text, View, Image, Alert,
+    Platform, TouchableHighlight, Dimensions,
+    TextInput
+} from 'react-native';
+import Modal from 'react-native-modalbox';
+import Button from 'react-native-button';
+import flatListData from '../data/flatListData';
+
+import { insertNewFoodToServer } from '../networking/Server';
+
+var screen = Dimensions.get('window');
+export default class AddModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newFoodName: '',
+            newFoodDescription: ''
+        };
+    }
+    showAddModal = () => {
+        this.refs.myModal.open();
+    }
+    generateKey = (numberOfCharacters) => {
+        return require('random-string')({length: numberOfCharacters});        
+    }
+    render() {
+        return (
+            <Modal
+                ref={"myModal"}
+                style={{
+                    justifyContent: 'center',
+                    borderRadius: Platform.OS === 'ios' ? 30 : 0,
+                    shadowRadius: 10,
+                    width: screen.width - 80,
+                    height: 280
+                }}
+                position='center'
+                backdrop={true}
+                onClosed={() => {
+                    // alert("Modal closed");
+                }}
+            >
+                <Text style={{
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    marginTop: 40
+                }}>New food's information</Text>
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderBottomColor: 'gray',
+                        marginLeft: 30,
+                        marginRight: 30,
+                        marginTop: 20,
+                        marginBottom: 10,
+                        borderBottomWidth: 1
+                    }}           
+                    onChangeText={(text) => this.setState({ newFoodName: text })}
+                    placeholder="Enter new food's name"
+                    value={this.state.newFoodName}                 
+                />
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderBottomColor: 'gray',
+                        marginLeft: 30,
+                        marginRight: 30,
+                        marginTop: 10,
+                        marginBottom: 20,
+                        borderBottomWidth: 1
+                    }}
+                    
+                    onChangeText={(text) => this.setState({ newFoodDescription: text })}
+                    placeholder="Enter new food's description"
+                    value={this.state.newFoodDescription}
+                />
+                <Button
+                    style={{ fontSize: 18, color: 'white' }}
+                    containerStyle={{
+                        padding: 8,
+                        marginLeft: 70,
+                        marginRight: 70,
+                        height: 40,
+                        borderRadius: 6,
+                        backgroundColor: 'mediumseagreen'
+                    }}
+                    onPress={() => {
+                         if (this.state.newFoodName.length == 0 || this.state.newFoodDescription.length == 0) {
+                            alert("You must enter food's name and description");
+                            return;
+                        }       
+                        const newKey = this.generateKey(24);
+                        const newFood = {
+                            key: newKey,
+                            name: this.state.newFoodName,
+                            // imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/64/Foods_%28cropped%29.jpg",
+                            foodDescription: this.state.newFoodDescription
+                        };    
+                        // flatListData.push(newFood);    
+                        // this.props.parentFlatList.refreshFlatList(newKey);          
+                        insertNewFoodToServer(newFood).then((result) => {
+                            if (result === 'ok') {
+                                this.props.parentFlatList.refreshDataFromServer();
+                            }
+                        });                  
+                        this.refs.myModal.close();                                                                       
+                    }}>
+                    Save
+                </Button>
+            </Modal>
+        );
+    }
+}
