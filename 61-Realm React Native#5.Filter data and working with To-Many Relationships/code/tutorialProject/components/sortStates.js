f09ab70ce45fb2139c7a4fe90efbe3105b837475
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-01-23 00:12:45 +0700

    update

--- 61-Realm React Native#5.Filter data and working with To-Many Relationships/code/tutorialProject/components/sortStates.js
+++
@@ -1,8 +0,0 @@
-/*
-Mr Nguyen Duc Hoang
-https://www.youtube.com/c/nguyenduchoang
-Email: sunlight4d@gmail.com
-List of sort states
-*/
-export const SORT_ASCENDING = "SORT_ASCENDING";
-export const SORT_DESCENDING = "SORT_DESCENDING";
