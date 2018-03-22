285885b29ae16a70f43f01ae75b4b1f68ddea857
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-03-22 08:20:06 +0700

    update

---
+++ 68-React Native Animations#3. Animate multiple Views with Stagger and Fade/code/ReactNativeProject/index.js
@@ -0,0 +1,10 @@
+/*
+Mr Nguyen Duc Hoang
+https://www.youtube.com/c/nguyenduchoang
+Programming tutorial channel
+*/
+import { AppRegistry } from 'react-native';
+// import App from './App';
+import StaggerComponent from './StaggerComponent';
+
+AppRegistry.registerComponent('ReactNativeProject', () => StaggerComponent);
